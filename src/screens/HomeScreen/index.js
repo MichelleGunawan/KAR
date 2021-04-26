5d83311fa2ed4c6d43c129e846f4acdc68787a04
Michelle Gunawan <michellegunawan9999@outlook.com>
2021-04-26 00:16:08 -0700

    amplify revision

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -8,12 +8,12 @@ import HomeSearch from '../../components/HomeSearch'
 const HomeScreen =(props) =>{
     return(
         <View>
-            <View style = {{height: Dimensions.get('window').height - 400}}>
+            <View style = {{height: Dimensions.get('window').height - 250}}>
                 <HomeMap />
             </View>
             
-            <View style = {{height: 400}}>
-                <CovidMessage/>
+            <View style = {{height: 250}}>
+                {/* <CovidMessage/> */}
                 <HomeSearch/>
             </View>
             
